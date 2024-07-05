e9ab2edd17b8ade71e96de8003d53e4e2abee294
EC2 Default User <ec2-user@ip-172-31-7-77.ap-northeast-1.compute.internal>
2024-07-05 06:00:53 +0000

    20240705

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,19 +18,24 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+     flash[:notice] = "You have updated book successfully."
+     redirect_to book_path(@book.id)
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "You have destroyed book successfully."
     redirect_to books_path
   end
 
